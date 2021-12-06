b9f8836cfe7f09e4880dc87e868ede49082393dc
manuelperez0000 <manuelperez.0000@gmail.com>
2021-12-06 09:03:32 -0400

    inicio para context

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -182,7 +182,7 @@ function Sidebar() {
 
                                     <li>
                                         <Link to="/planet" className="nav-button">
-                                            • Planets
+                                            • Planets x
                                         </Link>
                                     </li>
 
@@ -199,10 +199,30 @@ function Sidebar() {
                                             <img className="logo-sidebar" src={logo} /> {glx}
                                         </div>
                                         <div className="">
-                                            <img className="logo-sidebar" src={gem} /> {user.gm}
+                                            <img className="logo-sidebar" src={gem} /> {user.gm} xxx
                                         </div>
                                     </div>
                                 </div>
+
+                                <div className="w-gems mt-4">
+                                <h4 className="text-white text-center">Exchange </h4>
+
+                                <div onClick={handleShow} className="gems-exchange d-flex justify-content-between">
+
+                                    <div className="d-inline-block">
+                                        <img className="img-gem" src={bnbLogo} />
+                                    </div>
+                                    <div className="d-inline-block">
+                                        <img className="img-gem" src={arrow} />
+                                    </div>
+                                    <div className="d-inline-block">
+                                        <img className="img-gem" src={gem} />
+                                    </div>
+                                </div>
+
+                            </div>
+
+
                             </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
@@ -215,7 +235,7 @@ function Sidebar() {
                         <ul className="nav nav-pills flex-column mb-auto">
                             <li>
                                 <Link to="/inventory" className="nav-button">
-                                    • Inventory
+                                    • Inventory x
                                 </Link>
                             </li>
                             <li>
@@ -257,7 +277,7 @@ function Sidebar() {
                                 </div>
                             </div>
                             <div className="w-gems mt-4">
-                                <h4 className="text-white text-center">Exchange</h4>
+                                <h4 className="text-white text-center">Exchange </h4>
 
                                 <div onClick={handleShow} className="gems-exchange d-flex justify-content-between">
 
@@ -277,6 +297,64 @@ function Sidebar() {
                     </div>
                 </div>
             </div>
+            <Modal className="bg-modal" show={show} onHide={handleClose}>
+                <Modal.Header className="modal-w text-white hr-modal" closeButton>
+                    <Modal.Title>
+                        <div className="d-flex justify-content-between w-100">
+                            <div>
+                                <div className="d-inline-block">
+                                    <img className="img-gem" src={bnbLogo} />
+                                </div>
+                                <div className="d-inline-block mx-3">
+                                    <img className="img-gem" src={arrow} />
+                                </div>
+                                <div className="d-inline-block">
+                                    <img className="img-gem" src={gem} />
+                                </div>
+                            </div>
+                        </div>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="modal-w text-center">
+
+                    <h4 className="text-white">1000 GM = 0.01 BNB</h4>
+                    <h4 className="text-white">3000 GM = 0.025 BNB</h4>
+                    <div className="modal-w py-4 d-flex justify-content-center">
+                        <div className="border p-3 m-2">
+                            <div className="text-center">
+                                <img height="50px" src={gem} alt="" />
+                            </div>
+                            <div className="text-white text-center mb-3">
+                                Gem Pack (1000 GM)
+                            </div>
+                            <button className="btn btn-success px-4" onClick={() => buyGm(1000, "0.01")}>
+                                Buy 1000 GM
+                            </button>
+                        </div>
+                        <div className="border p-3 m-2">
+                            <div className="text-center">
+                                <img height="50px" src={gem} alt="" />
+                            </div>
+                            <div className="text-white text-center mb-3">
+                                Gem Pack (3000 GM)
+                            </div>
+                            <button className="btn btn-success px-4" onClick={() => buyGm(3000, "0.025")}>
+                                Buy 3000 GM
+                            </button>
+                        </div>
+                    </div>
+
+
+
+                </Modal.Body>
+                <Modal.Footer className="modal-w text-center">
+                    <div className="text-center w-100">
+
+                    </div>
+                </Modal.Footer>
+            </Modal>
+
+
         </div>
     )
 }
